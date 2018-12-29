9ae3ec67f66160c890381ec920b8a06426b3e1e3
Timothy Tong <tktong418@gmail.com>
2018-12-29 13:20:50 -0800

    Add more asserttimeout examples

--- src/test/java/me/tktong/annotations/timeout/TimeoutJUnit5Test.java
+++ src/test/java/me/tktong/annotations/timeout/TimeoutJUnit5Test.java
@@ -6,13 +6,51 @@ import org.junit.jupiter.api.Test;
 import java.time.Duration;
 
 class TimeoutJUnit5Test {
+    private static final String MESSAGE = "My message";
+    private static final String RETURN_VALUE = "Some return value";
+    private static final Duration TIMEOUT_DURATION = Duration.ofMillis(10L);
+
     @Test
-    void timeoutTest() {
-        Assertions.assertTimeout(Duration.ofMillis(10L), () -> someBusinessLogic(1000L));
+    void assertTimeout() {
+        Assertions.assertTimeout(TIMEOUT_DURATION, () -> someBusinessLogic(1000L));
+    }
+
+    @Test
+    void assertTimeoutWithMessage() {
+        Assertions.assertTimeout(TIMEOUT_DURATION, () -> someBusinessLogic(1000L), MESSAGE);
+    }
+
+    @Test
+    void assertTimeoutWithMessageSupplier() {
+        Assertions.assertTimeout(TIMEOUT_DURATION, () -> someBusinessLogic(1000L), () -> MESSAGE);
+    }
+
+    @Test
+    void assertTimeoutWithThrowableSupplier() {
+        final String result = Assertions.assertTimeout(TIMEOUT_DURATION, () -> someBusinessLogicWithReturnValue(100L));
+        Assertions.assertEquals(RETURN_VALUE, result);
+    }
+
+    @Test
+    void assertTimeoutWithThrowableSupplierAndMessage() {
+        final String result = Assertions.assertTimeout(TIMEOUT_DURATION, () -> someBusinessLogicWithReturnValue(100L), MESSAGE);
+        Assertions.assertEquals(RETURN_VALUE, result);
+    }
+
+    @Test
+    void assertTimeoutWithThrowableSupplierAndMessageSupplier() {
+        final String result = Assertions.assertTimeout(TIMEOUT_DURATION, () -> someBusinessLogicWithReturnValue(100L), () -> MESSAGE);
+        Assertions.assertEquals(RETURN_VALUE, result);
     }
 
     private void someBusinessLogic(final long sleepDuration)
             throws InterruptedException{
         Thread.sleep(sleepDuration);
+    }
+
+    private String someBusinessLogicWithReturnValue(final long sleepDuration)
+            throws InterruptedException{
+        Thread.sleep(sleepDuration);
+        return RETURN_VALUE;
     }
 }
